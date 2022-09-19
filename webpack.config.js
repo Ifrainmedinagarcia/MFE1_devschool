0970348000d6c418533c085b60998dd0d8044d5d
Ifrain <imedinag@achs.cl>
2022-09-19 18:27:05 -0300

    fix remotes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = (env, arg) => {
         name: "clothes",
         filename: "remoteEntry.js",
         remotes: {
-          "app_container": "app_container@https://ifrainmedinagarcia.github.io/app_container_devschool/",
+          "app_container": "app_container@https://ifrainmedinagarcia.github.io/app_container_devschool/remoteEntry.js",
         },
         exposes: {
           "./App": "./src/App"
